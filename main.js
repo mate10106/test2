554053927cfbc04ef3e5dfa745ab9fe7370e77da
Mate Brosztl <brosztl.mate@gmail.com>
2023-09-16 16:39:05 +0200

    add some code here

--- main.js
+++ main.js
@@ -4,3 +4,14 @@ document.getElementById("myButton").onclick = function(){
     document.getElementById("welcome").innerHTML = "Welcome here dear " + name;
 
 };
+
+
+function maxNum(n1,n2){
+    if (n1<n2) {
+        return n2;
+    }
+    else{
+        return n1;
+    }
+}
+console.log(maxNum(2,12))
